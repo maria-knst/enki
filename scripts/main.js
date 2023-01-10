350ad034e48653e140f25a6805bc3717b74fd04a
Maria <konstantinovamaria25@gmail.com>
2023-01-10 16:17:56 +0300

    Fix - Fix images, add modal

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,48 @@ showMoreBut.addEventListener("click", (e) => {
   });
   showMoreBut.classList.add("activities_show-more-hidden");
 });
+
+
+const modal = document.getElementById('about_icon-modal');
+const showButton = document.getElementById('show-modal');
+
+const modalContainer = document.getElementById('about-modal-container');
+const closeModal = document.getElementById('close-modal');
+const arrowPrev = document.getElementById('arrow-prev');
+const arrowNext = document.getElementById('arrow-next');
+
+modalContainer.innerHTML = '<img src="./images/certificates/certificate1.jpg" alt="certificate">';
+let index = 1;
+
+arrowPrev.addEventListener('click', (event) => {
+  event.preventDefault();
+  if(index <= 1){
+    index = 3;
+  }
+  else {
+    index--;
+  }
+  modalContainer.innerHTML = `<img src="./images/certificates/certificate${index}.jpg" alt="certificate">`;
+});
+
+arrowNext.addEventListener('click', (event) => {
+  event.preventDefault();
+  if(index >= 3){
+    index = 1;
+  }
+  else {
+    index++;
+  }
+  modalContainer.innerHTML = `<img src="./images/certificates/certificate${index}.jpg" alt="certificate">`;
+});
+
+closeModal.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log('CLOSE');
+  modal.classList.toggle('hidden-element');
+});
+
+showButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  modal.classList.toggle('hidden-element');
+});
