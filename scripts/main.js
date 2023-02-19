9d95e55998dacb0927c9441085dba76a24e62d7b
Maria <konstantinovamaria25@gmail.com>
2023-02-19 12:22:28 +0300

    Feat - Add Swiper JS in project

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
+
 const activities = document.querySelectorAll(".activities_container-element");
 const activities_m = document.querySelectorAll(".activities_container-element-m");
 const list_items = document.querySelectorAll(".header-menu_list-item_clicked");
 const showMoreBut = document.getElementById("activities_show-more");
+
+window.addEventListener('resize', (e) => {
+  location.reload();
+});
 
 if(window.innerWidth < 1200){
   showMoreBut.innerHTML = '<img src="./images/arrow.svg" alt="arrow">';
@@ -31,15 +37,8 @@ list_items.forEach(item => {
 const modal = document.getElementById('about_icon-modal');
 const showButton = document.getElementById('show-modal');
 
-const modalContainer = document.getElementById('about-modal-container');
 const closeModal = document.getElementById('close-modal');
-const arrowPrev = document.getElementById('arrow-prev');
-const arrowNext = document.getElementById('arrow-next');
 
-modalContainer.innerHTML = '<img src="./images/certificates/certificate1.png" alt="certificate">';
-
-let index = 1;
-const startCertificateIndex = 1;
 const lastCertificateIndex = 9;
 
 const getPagination = (element, number, light) => {
@@ -52,24 +51,8 @@ const getPagination = (element, number, light) => {
   }
 }
 
-getPagination(document.getElementById('modal-pagination'),lastCertificateIndex, true);
 getPagination(document.getElementById('about-pagination'),lastCertificateIndex);
 
-arrowPrev.addEventListener('click', (event) => {
-  event.preventDefault();
-  document.getElementById(`pag${index}`).classList.remove('about_pag_selected_item');
-  index = (index <= startCertificateIndex) ? lastCertificateIndex : --index;
-  modalContainer.innerHTML = `<img src="./images/certificates/certificate${index}.png" alt="certificate">`;
-  document.getElementById(`pag${index}`).classList.add('about_pag_selected_item');
-});
-
-arrowNext.addEventListener('click', (event) => {
-  event.preventDefault();
-  document.getElementById(`pag${index}`).classList.remove('about_pag_selected_item');
-  index = (index >= lastCertificateIndex) ? startCertificateIndex : ++index;
-  modalContainer.innerHTML = `<img src="./images/certificates/certificate${index}.png" alt="certificate">`;
-  document.getElementById(`pag${index}`).classList.add('about_pag_selected_item');
-});
 
 closeModal.addEventListener('click', (event) => {
   event.preventDefault();
@@ -147,53 +130,36 @@ document.getElementById('menu_item_address_p').addEventListener('click', (event)
 });
 
 
-const sliders = [
-
-  '<div class="info-element">\n' +
-  '              <h4 class="info-element_title">Опыт</h4>\n' +
-  '              <p class="info-element_text">\n' +
-  '                В нашей компании работают сотрудники со стажем в сфере\n' +
-  '                строительства 15+ лет\n' +
-  '              </p>\n' +
-  '            </div>',
-
-  '<div class="info-element">\n' +
-  '              <h4 class="info-element_title">Качество</h4>\n' +
-  '              <p class="info-element_text">\n' +
-  '                Мы полностью уверены в качестве выполненной работы, поэтому\n' +
-  '                предоставляем длительную гарантию\n' +
-  '              </p>\n' +
-  '            </div>',
-
-  '<div class="info-element">\n' +
-  '              <h4 class="info-element_title">Время</h4>\n' +
-  '              <p class="info-element_text">\n' +
-  '                Мы всегда выполняем работы точно в срок, указанный в договоре.\n' +
-  '                Мы ценим Ваше время\n' +
-  '              </p>\n' +
-  '            </div>',
-
-  '<div class="info-element">\n' +
-  '              <h4 class="info-element_title">Развитие</h4>\n' +
-  '              <p class="info-element_text">\n' +
-  '                Мы следим за новыми тенденциями в сфере строительства и\n' +
-  '                своевременно включаем их в свою работу\n' +
-  '              </p>\n' +
-  '            </div>',
-];
-
-let slideIndex = 0;
-const benefitsSlider = document.getElementById('benefits-section_info-container');
-
 if(window.innerWidth <= 1200 && window.innerWidth >= 720) {
-  setInterval(() => {
-    if(slideIndex === sliders.length - 1){
-      benefitsSlider.innerHTML = sliders[slideIndex] + sliders[0];
-      slideIndex = 0;
-    }else {
-      benefitsSlider.innerHTML = sliders[slideIndex] + sliders[slideIndex + 1];
-      slideIndex++;
-    }
-  }, 3000);
+  document.getElementById('benefits-section_info-container').classList.add('benefits-swiper');
+  document.getElementById('benefits-section_info-container').classList.add('swiper');
+  document.getElementById('swiper-wrapper').classList.add('swiper-wrapper');
+  document.querySelectorAll('.info-element').forEach(item => {
+    item.classList.add('swiper-slide');
+  });
+
 }
+
+const swiper1 = new Swiper(".benefits-swiper", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 4000,
+  },
+});
+
+
+const swiper2 = new Swiper(".modal-swiper", {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
 
